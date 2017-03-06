62048085f61fc842b6b9dde2c3b76990cf3ca930
zg <1075120274@qq.com>
2017-03-06 20:19:59 +0800

    添加了TV

--- app/src/main/java/itheima/myapplication/MainActivity.java
+++ app/src/main/java/itheima/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package itheima.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         View viewById = findViewById(R.id.activity_main);
+        View f = findViewById(R.id.activity_main);
+
         View viewById2 = findViewById(R.id.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText("呵呵。。。");
     }
 }
