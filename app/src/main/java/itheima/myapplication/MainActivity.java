e310ef83524fc57d27a6f8d42e47a5370426a6bf
zg <1075120274@qq.com>
2017-03-06 19:37:16 +0800

    2

--- app/src/main/java/itheima/myapplication/MainActivity.java
+++ app/src/main/java/itheima/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package itheima.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View viewById = findViewById(R.id.activity_main);
+        View viewById2 = findViewById(R.id.activity_main);
     }
 }
